0c4fb7e7db7bffd5069c1f92f8369f6a0863bb9d
Vladislav Zhukov <vladislavzhukov@MacBook-Pro-Vladislav-2.local>
2026-04-26 21:02:48 +0300

    init xcode project

---
+++ FreeFolder/FreeFolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeFolderApp.swift
+//  FreeFolder
+//
+//  Created by Vladislav Zhukov on 26.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FreeFolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
